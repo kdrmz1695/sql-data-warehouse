d879fa33bd34c604046e5616a798868d174bd642
kdrmz1695 <k.durmaz1695@gmail.com>
2026-04-13 23:02:03 +0200

    create gold layer
    this script creates gold layer and it represent final dimension and fact tables (star schema)

---
+++ scripts/gold/gold layer.sql
@@ -0,0 +1,75 @@
+/*
+===========================================================
+
+DDL Script: Create Gold Views
+
+===========================================================
+This script creates views for the Gold Layer and it represents the final dimension
+and fact tables (star schema)
+
+
+Usage :
+
+These views can be queried directly for analytics and reporting
+
+============================================================
+*/
+
+CREATE VIEW gold.dim_customers AS 
+SELECT
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, --burasi surrogate key
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	ci.cst_marital_status AS marital_status,
+	ci.cst_create_date AS create_date,
+	ca.bdate AS birth_date,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- ana tablodaki genderi baz al.
+	ELSE COALESCE(ca.gen, 'n/a')
+	END AS new_gen,
+	la.cntry AS country
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid
+
+
+
+
+CREATE VIEW gold.dim_products AS
+SELECT
+ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+pn.prd_id AS product_id,
+pn.cat_id AS category_id,
+pn.prd_key AS product_number,
+pn.prd_nm AS product_name,
+pn.prd_cost AS cost,
+pn.prd_line AS product_line,
+pn.prd_start_dt AS start_date,
+pc.cat AS category,
+pc.subcat AS sub_category,
+pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL
+
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+sd.sls_ord_num AS order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shipping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales_amount,
+sd.sls_quantity AS quantity,
+sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id
